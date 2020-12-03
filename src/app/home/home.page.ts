78cdc616c8a4091c91bcd0604ab8e2c1c00122a7
aymen <aymen.elhajayed@gmail.com>
2020-12-03 14:27:21 +0100

    Add images list with infinite scroll

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ImageModel} from './image.model';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    imagesList: ImageModel[] = [];
+    page = 1;
+    limit = 10;
+    disableInfiniteScroll = false;
+
+    constructor(private http: HttpClient) {
+        this.loadImages();
+    }
+
+    loadImages(event?) {
+        this.http.get(`https://picsum.photos/v2/list?page=${this.page}&limit=${this.limit}`).subscribe(
+            (res: ImageModel[]) => {
+                this.imagesList = this.imagesList.concat(res);
+                if (event) {
+                    event.target.complete();
+                }
+            },
+            error => {
+            }
+        );
+    }
+
+    loadMoreImages(event) {
+        this.page++;
+        this.loadImages(event);
+        if (this.disableInfiniteScroll) {
+            event.target.disable = true;
+        }
+    }
 
 }
