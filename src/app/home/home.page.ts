00f510a78e6e3dae764b627a6658a84a1ecb727a
aymen <aymen.elhajayed@gmail.com>
2020-12-03 15:13:57 +0100

    Add search bar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,7 @@ import {ImageModel} from './image.model';
 export class HomePage {
 
     imagesList: ImageModel[] = [];
+    imagesListBackup: ImageModel[];
     page = 1;
     limit = 10;
     disableInfiniteScroll = false;
@@ -22,6 +23,7 @@ export class HomePage {
         this.http.get(`https://picsum.photos/v2/list?page=${this.page}&limit=${this.limit}`).subscribe(
             (res: ImageModel[]) => {
                 this.imagesList = this.imagesList.concat(res);
+                this.imagesListBackup = this.imagesList;
                 if (event) {
                     event.target.complete();
                 }
@@ -37,6 +39,25 @@ export class HomePage {
         if (this.disableInfiniteScroll) {
             event.target.disable = true;
         }
+    }
+
+    async filterList(evt) {
+        this.imagesList = this.imagesListBackup;
+        const searchTerm = evt.srcElement.value;
+
+        if (!searchTerm) {
+            return;
+        }
+
+        this.imagesList = this.imagesList.filter(currentImage => {
+            if (currentImage.author && searchTerm) {
+                return (currentImage.author.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1);
+            }
+        });
+    }
+
+    detectChangeSearchBar(event) {
+        this.disableInfiniteScroll = !!event.detail.value;
     }
 
 }
